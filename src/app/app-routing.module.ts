bae210843874d6dfa03a42462986bdfe75f48e33
Sergio <setkode@hotmail.com>
2019-03-21 13:10:12 -0600

    Generación de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import{HomePageComponent} from "./componentes/home-page/home-page.component";
+import {LoginPageComponent} from "./componentes/login-page/login-page.component";
+import {RegisterPageComponent} from "./componentes/register-page/register-page.component";
+import {PrivadoPageComponent} from "./componentes/privado-page/privado-page.component";
+import {NotfoundPageComponent} from "./componentes/notfound-page/notfound-page.component";
+const routes: Routes = [
+{path: '', component: HomePageComponent},
+{path: 'login', component: LoginPageComponent},
+{path: 'register', component: RegisterPageComponent},
+{path: 'privado', component: PrivadoPageComponent},
+{path: '**', component: NotfoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
