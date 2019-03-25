077d02d265debff8c590a6f378ade23ebf754e17
Sergio <setkode@hotmail.com>
2019-03-24 18:31:51 -0600

    instalacion de firebase y configuracion environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAmz6OpijTdPYg8t_dujC9qnnwtP4mam9E",
+    authDomain: "fecer-b0517.firebaseapp.com",
+    databaseURL: "https://fecer-b0517.firebaseio.com",
+    projectId: "fecer-b0517",
+    storageBucket: "fecer-b0517.appspot.com",
+    messagingSenderId: "971286580108"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAmz6OpijTdPYg8t_dujC9qnnwtP4mam9E",
+    authDomain: "fecer-b0517.firebaseapp.com",
+    databaseURL: "https://fecer-b0517.firebaseio.com",
+    projectId: "fecer-b0517",
+    storageBucket: "fecer-b0517.appspot.com",
+    messagingSenderId: "971286580108"
+
+  }
 };
 
 /*
